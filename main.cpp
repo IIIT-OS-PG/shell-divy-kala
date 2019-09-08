2fb2b766f47187027bb194cdaeac8dfcc63ad481
divy-kala <divy.kala@students.iiit.ac.in>
2019-09-08 16:15:58 +0530

    Record implemented for pipes

--- main.cpp
+++ main.cpp
@@ -282,7 +282,7 @@ void run_double_redirect ( char ** rargs, int tokcount)
     {
 
 
-        int fd = open(rargs[tokcount-1], O_APPEND | O_WRONLY);
+        int fd = open(rargs[tokcount-1], O_APPEND | O_WRONLY | O_CREAT, S_IRWXU);
         dup2(fd,1);
         close(fd);
         string fullcom = path + rargs[0];
@@ -375,15 +375,15 @@ void run_command(char ** trueargs, int tokcount)
 
         }
         close(pip[0]);
-         waitpid(ret,NULL,0);
+        waitpid(ret,NULL,0);
         waitpid(k,NULL,0);
         //    cout << "k term";
 
     }
     else
     {
-            close(pip[1]);
-    close(pip[0]);
+        close(pip[1]);
+        close(pip[0]);
         waitpid(ret,NULL,0);
         //  cout << "ret term";
     }
@@ -397,6 +397,10 @@ void run_command(char ** trueargs, int tokcount)
 void run_pipes (char ** rargs, int tokcount, int pipes)
 {
 
+    int pip[2];
+    pipe(pip);
+
+
     char * cmds[pipes+1][MAXARGS];
 
     int cmdi = 0;
@@ -404,6 +408,173 @@ void run_pipes (char ** rargs, int tokcount, int pipes)
     for(int i =0 ; i < tokcount; i++)
     {
         if(strcmp(rargs[i], "|") == 0)
+        {
+            cmds[cmdi][j] = NULL;
+            j=0;
+            cmdi++;
+
+
+            continue;
+        }
+
+        cmds[cmdi][j++] = rargs[i];
+
+
+    }
+    cmds[cmdi][j] = NULL;
+
+    int cmdnum = cmdi+1;
+
+    int mypipe[pipes][2];
+    for(int i = 0; i < pipes; i++ )
+    {
+        pipe(mypipe[i]);
+    }
+    vector<int> child_pids;
+
+    for(int i = 0; i < cmdnum; i++ )
+    {
+        int f = fork();
+
+        if(f==-1)
+        {
+            cout << "couldn't fork";
+
+        }
+        if ( f != 0)
+        {
+
+            child_pids.push_back(f);
+            //  cout << "process pushed : " << f << endl;
+        }
+        if(f==0)
+        {
+
+            if(i==0)
+            {
+                dup2(mypipe[0][1], 1);
+//                close (mypipe[0][1]);
+//                close (mypipe[0][1]);
+
+            }
+            else if (i > 0 && i < cmdnum-1)
+            {
+                dup2(mypipe[i-1][0], 0);
+//                close(mypipe[i-1][0]);
+                dup2(mypipe[i][1],1);
+//                close(mypipe[i][1]);
+//                close(mypipe[i][0]);
+
+            }
+
+            else
+            {
+
+                if(record)
+                {
+                    dup2(pip[1],1);
+
+                }
+
+                dup2(mypipe[i-1][0],0);
+//                close(mypipe[i-1][0]);
+//                close(mypipe[i-1][1]);
+
+            }
+            close(pip[1]);
+            close(pip[0]);
+            for(int i = 0; i < pipes; i++ )
+            {
+
+                close(mypipe[i][0]);
+                close(mypipe[i][1]);
+            }
+
+            string fullcom = path + cmds[i][0];
+
+            if( execv(fullcom.c_str(), cmds[i]) == -1)
+            {
+                cout << "\ncommand could not be executed\n";
+                exit(-1);
+            }
+        }
+
+    }
+    close(pip[1]);
+    for(int i = 0; i < pipes; i++ )
+    {
+        close(mypipe[i][0]);
+        close(mypipe[i][1]);
+    }
+
+
+
+    if(record)
+    {
+
+        int k = fork();
+        if(k==0)
+        {
+
+            char c[100];
+            int count;
+
+            int con;
+
+            //   while (ioctl(pip[0], FIONREAD, &con) == 0 && con > 0)
+            while ( (count = read(pip[0], c,100) ) > 0 )
+            {
+                write(scriptfile, c, count);
+                write(1,c,count);
+            }
+
+            close(pip[0]);
+            _exit(0);
+
+        }
+
+
+        //    cout << "k term";
+
+    }
+    close(pip[0]);
+
+
+    for(auto i = child_pids.begin(); i != child_pids.end(); i++)
+    {
+        //  cout << "waitig for " << *i <<endl;
+//        if(i==child_pids.back) {
+//            kill(*i, 0);
+//        }
+        waitpid(*i,NULL,0);
+    }
+
+
+
+    // while(wait(NULL) != -1) ;
+//    wait(NULL);
+
+
+//TODO delete pipes
+
+
+
+}
+
+void redirect_from_pipe (char ** rargs, int tokcount, int pipes)
+{
+
+
+    char * cmds[pipes+1][MAXARGS];
+    string filepath = rargs[tokcount-1];
+    tokcount = tokcount -2;
+
+    int  writefile = open(filepath.c_str(), O_WRONLY | O_CREAT| O_TRUNC, S_IRWXU);
+    int cmdi = 0;
+    int j = 0;
+    for(int i =0 ; i < tokcount; i++)
+    {
+        if(strcmp(rargs[i], "|" ) == 0 || strcmp(rargs[i], ">" ) == 0)
         {
             cmds[cmdi][j] = NULL;
             j=0;
@@ -464,14 +635,18 @@ void run_pipes (char ** rargs, int tokcount, int pipes)
             {
 
                 dup2(mypipe[i-1][0],0);
+                dup2(writefile, 1);
+
 //                close(mypipe[i-1][0]);
 //                close(mypipe[i-1][1]);
 
             }
+            close(writefile);
             for(int i = 0; i < pipes; i++ )
             {
                 close(mypipe[i][0]);
                 close(mypipe[i][1]);
+
             }
 
             string fullcom = path + cmds[i][0];
@@ -490,7 +665,7 @@ void run_pipes (char ** rargs, int tokcount, int pipes)
         close(mypipe[i][1]);
     }
 
-
+    close(writefile);
     for(auto i = child_pids.begin(); i != child_pids.end(); i++)
     {
         //  cout << "waitig for " << *i <<endl;
@@ -511,6 +686,133 @@ void run_pipes (char ** rargs, int tokcount, int pipes)
 
 
 }
+
+void dredirect_from_pipe (char ** rargs, int tokcount, int pipes)
+{
+
+
+    char * cmds[pipes+1][MAXARGS];
+    string filepath = rargs[tokcount-1];
+    tokcount = tokcount -2;
+
+    int  writefile = open(filepath.c_str(), O_APPEND | O_CREAT | O_WRONLY, S_IRWXU);
+    int cmdi = 0;
+    int j = 0;
+    for(int i =0 ; i < tokcount; i++)
+    {
+        if(strcmp(rargs[i], "|" ) == 0 || strcmp(rargs[i], ">>" ) == 0)
+        {
+            cmds[cmdi][j] = NULL;
+            j=0;
+            cmdi++;
+
+
+            continue;
+        }
+
+        cmds[cmdi][j++] = rargs[i];
+
+
+    }
+    cmds[cmdi][j] = NULL;
+
+    int cmdnum = cmdi+1;
+
+    int mypipe[pipes][2];
+    for(int i = 0; i < pipes; i++ )
+    {
+        pipe(mypipe[i]);
+    }
+    vector<int> child_pids;
+
+    for(int i = 0; i < cmdnum; i++ )
+    {
+        int f = fork();
+        if(f==-1)
+        {
+            cout << "couldn't fork";
+
+        }
+        if ( f != 0)
+        {
+            child_pids.push_back(f);
+            //  cout << "process pushed : " << f << endl;
+        }
+        if(f==0)
+        {
+            if(i==0)
+            {
+                dup2(mypipe[0][1], 1);
+//                close (mypipe[0][1]);
+//                close (mypipe[0][1]);
+
+            }
+            else if (i > 0 && i < cmdnum-1)
+            {
+                dup2(mypipe[i-1][0], 0);
+//                close(mypipe[i-1][0]);
+                dup2(mypipe[i][1],1);
+//                close(mypipe[i][1]);
+//                close(mypipe[i][0]);
+
+            }
+
+            else
+            {
+
+                dup2(mypipe[i-1][0],0);
+                dup2(writefile, 1);
+
+//                close(mypipe[i-1][0]);
+//                close(mypipe[i-1][1]);
+
+            }
+            for(int i = 0; i < pipes; i++ )
+            {
+                close(mypipe[i][0]);
+                close(mypipe[i][1]);
+
+            }
+            close(writefile);
+            string fullcom = path + cmds[i][0];
+
+            if( execv(fullcom.c_str(), cmds[i]) == -1)
+            {
+                cout << "\ncommand could not be executed\n";
+                exit(-1);
+            }
+        }
+
+    }
+    for(int i = 0; i < pipes; i++ )
+    {
+        close(mypipe[i][0]);
+        close(mypipe[i][1]);
+    }
+
+    close(writefile);
+    for(auto i = child_pids.begin(); i != child_pids.end(); i++)
+    {
+        //  cout << "waitig for " << *i <<endl;
+//        if(i==child_pids.back) {
+//            kill(*i, 0);
+//        }
+        waitpid(*i,NULL,0);
+    }
+
+
+
+    // while(wait(NULL) != -1) ;
+//    wait(NULL);
+
+
+//TODO delete pipes
+
+
+
+}
+
+
 
 
 int main()
@@ -857,6 +1159,17 @@ int main()
                     chdir(  rargs[1]);
 
                 }
+
+            }
+            else if (rediri ==1 && pipes > 0)
+            {
+                redirect_from_pipe(rargs,tokcount,pipes);
+
+            }
+            else if (dredir ==1 && pipes > 0)
+            {
+                dredirect_from_pipe(rargs,tokcount,pipes);
+
 
             }
             else if (dredir == 1 )
